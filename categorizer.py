30aed4e10f0202ec51faf103f73d811d39a99eb9
Michelle Heffelfinger <miheffelfinger@godaddy.com>
2017-09-26 15:55:17 -0400

    Completed for loop

--- categorizer.py
+++ categorizer.py
@@ -1,12 +1,21 @@
 from iris_helper import Iris_helper
+from settings import dbstring
 
-#ToDo instantiate the Iris_helper Class
-
+i = Iris_helper(dbstring)
 
 def cleanup():
     #ToDo run trash_man and call ticket close
-    pass
+    address = ['sh.baidu.com', 'notices.nr-online.com', 'legal-notification.com', 'woody.ch', 'justdeals.com',
+                   'certifiedmart.com', 'marcon-media.de', 'newsletter.kopp-verlag.de', 'brandshop.com', 'law360.com',
+                   'ogrupo.org.br', 'domainerschoice.com', '163.com', 'peakindustry.com', 'woody.ch', 'foxmail.com',
+                   'sina.cn', 'sina.com', 'appleitunesguide.com', 'notice.bizcn.com']
+    garbage = []
+    for a in address:
+        trash = i.trash_man(a)
+        for t in trash:
+            garbage.append(t[0])
 
+    print garbage
 
 def categorize():
     #ToDo logic to check summary for keywords and create lists of IIDs to "move" Check out import re
