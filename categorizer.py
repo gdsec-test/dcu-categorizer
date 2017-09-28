bb32988f1e05d3a90d7ddb39d4e95da233be054e
jharris <jharris@godaddy.com>
2017-09-28 13:36:57 -0700

    Added pwd env var, solo run, setup category lists and keywords.

--- categorizer.py
+++ categorizer.py
@@ -4,7 +4,6 @@ from settings import dbstring
 i = Iris_helper(dbstring)
 
 def cleanup():
-    #ToDo run trash_man and call ticket close
     address = ['sh.baidu.com', 'notices.nr-online.com', 'legal-notification.com', 'woody.ch', 'justdeals.com',
                    'certifiedmart.com', 'marcon-media.de', 'newsletter.kopp-verlag.de', 'brandshop.com', 'law360.com',
                    'ogrupo.org.br', 'domainerschoice.com', '163.com', 'peakindustry.com', 'woody.ch', 'foxmail.com',
@@ -19,28 +18,23 @@ def cleanup():
 
 def categorize():
     #ToDo logic to check summary for keywords and create lists of IIDs to "move" Check out import re
+    print i.data_pull()
+    phish_cat = []
+    malware_cat =[]
+    netabuse_cat = []
+    spam_cat = []
+    garbage_cat = []
+
+    phish_keys = ['phishing', 'phish']
+    malware_keys = ['malware', 'virus']
+    netabuse_keys = ['botnet', 'intrusion', 'scan', 'attempted login', 'login attempted']
+    spam_keys = ['spam', 'spoof', 'spoofed']
+    garbage_keys = ['copyright', 'trademark', 'infringement']
 
     #ToDo setup regex seperated by category for use in this function
-    """
-    Keywords (in priority):
-	Phishing (P)
-	Phish (P)
-	SPAM (S)
-	Malware (M)
-	Virus (M)
-	Botnet (NA)
-	Scan (NA)
-	Scam (P)
-	Intrusion (NA)
-	attempted login (NA)
-	login attempted (NA)
-	Spoof (S)
-	Fake (P)
-	Copy (P)
-	Copyright (Close)
-	Trademark (Close)
-	Infringement (Close)
-    """
+
+
     pass
 
-#ToDo setup script to run
+if __name__ == '__main__':
+    categorize()
--- settings.py
+++ settings.py
@@ -1,4 +1,8 @@
+import os
 
 #ToDo set pwd to environmental variable
+
+
+pwd = os.getenv('IRISPWD') or 'password'
 
 dbstring = 'DRIVER={FreeTDS};SERVER=10.32.146.30;PORT=1433;DATABASE=iris;UID=N1_mF09EAA138D464E;PWD=' + pwd + ';TDS_VERSION=8.0'
