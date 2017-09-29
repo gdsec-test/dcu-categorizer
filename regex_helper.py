1dc9bcd1f075285839bf74ba29a3c5e2c197157d
Michelle Heffelfinger <miheffelfinger@godaddy.com>
2017-09-29 16:36:21 -0400

    created new class regex_helper.py

---
+++ regex_helper.py
@@ -0,0 +1,16 @@
+import re
+
+
+class List_helper():
+
+    def reg_logic(self, o_list, k_list):
+        results = []
+        for d in o_list:
+            for i in k_list:
+                if re.match(i, str(d[1])):
+                    results.append(d[0])
+                    o_list.remove(d)
+                else:
+                    print 'No Match ' + str(d) + ' on pattern ' + i
+        return results, o_list
+
