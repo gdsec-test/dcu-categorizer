7b90f354ae688a0fa55f618d8882ff4476937658
Patrick McConnell <pmcconnell@godaddy.com>
2017-11-21 10:20:23 -0800

    add logger to tests

--- tests/categorizer_tests.py
+++ tests/categorizer_tests.py
@@ -4,11 +4,23 @@ from nose.tools import assert_equal
 from categorizer.categorizer import Categorizer
 from categorizer.iris_helper import IrisHelper
 
+import logging.handlers
+
+
+# SET UP LOGGING
+
+log_handler = logging.handlers.RotatingFileHandler(filename='categorizer.log', backupCount=5, maxBytes=5 * 1024 * 1024)
+formatter = logging.Formatter("[%(levelname)s:%(asctime)s:%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s")
+log_handler.setFormatter(formatter)
+_logger = logging.getLogger(__name__)
+_logger.addHandler(log_handler)
+_logger.setLevel(logging.INFO)
+
 
 class TestCategorizer:
 
     def __init__(self):
-        self._cat = Categorizer()
+        self._cat = Categorizer(_logger)
 
     @patch.object(IrisHelper, 'data_pull')
     @patch.object(IrisHelper, 'ticket_move')
