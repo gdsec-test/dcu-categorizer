f90f0517ce8a33c5afadb6161fe10cac550df4b7
Patrick McConnell <pmcconnell@godaddy.com>
2017-11-21 08:19:58 -0800

    redid logging

--- run.py
+++ run.py
@@ -4,12 +4,12 @@ import logging.handlers
 
 # SET UP LOGGING
 
-logging.basicConfig(filename='categorizer.log', level=logging.INFO,
-                    format="[%(levelname)s:%(asctime)s:%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
-                    )
 log_handler = logging.handlers.RotatingFileHandler(filename='categorizer.log', backupCount=5, maxBytes=5 * 1024 * 1024)
+formatter = logging.Formatter("[%(levelname)s:%(asctime)s:%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s")
+log_handler.setFormatter(formatter)
 _logger = logging.getLogger(__name__)
 _logger.addHandler(log_handler)
+_logger.setLevel(logging.INFO)
 
 c = Categorizer()
 
