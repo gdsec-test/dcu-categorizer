f6827dbf483694b241a94a0291d9d080884de95a
Patrick McConnell <pmcconnell@godaddy.com>
2017-11-29 07:29:40 -0800

    add try/except/finally

--- categorizer/categorizer.py
+++ categorizer/categorizer.py
@@ -83,36 +83,42 @@ class Categorizer:
 
             incident_dict[iid] = (subject, body)
 
-        phish_cat = self.l.reg_logic(incident_dict, listings.phish_keys)
-        self._logger.info('Phishing incidents moved: {}'.format(phish_cat[0]))
-        self._move(settings.phish_service_id, phish_cat[0], settings.csa_group_id, self.eid)
-        buckets['phishing'] = phish_cat[0]
+        try:
 
-        mal_cat = self.l.reg_logic(phish_cat[1], listings.malware_keys)
-        self._logger.info('Malware incidents moved: {}'.format(mal_cat[0]))
-        self._move(settings.mal_service_id, mal_cat[0], settings.csa_group_id, self.eid)
-        buckets['malware'] = mal_cat[0]
+            phish_cat = self.l.reg_logic(incident_dict, listings.phish_keys)
+            self._logger.info('Phishing incidents moved: {}'.format(phish_cat[0]))
+            self._move(settings.phish_service_id, phish_cat[0], settings.csa_group_id, self.eid)
+            buckets['phishing'] = phish_cat[0]
 
-        net_cat = self.l.reg_logic(mal_cat[1], listings.netabuse_keys)
-        self._logger.info('Netabuse incidents moved: {}'.format(net_cat[0]))
-        self._move(settings.net_service_id, net_cat[0], settings.csa_group_id, self.eid)
-        buckets['netabuse'] = net_cat[0]
+            mal_cat = self.l.reg_logic(phish_cat[1], listings.malware_keys)
+            self._logger.info('Malware incidents moved: {}'.format(mal_cat[0]))
+            self._move(settings.mal_service_id, mal_cat[0], settings.csa_group_id, self.eid)
+            buckets['malware'] = mal_cat[0]
 
-        spam_cat = self.l.reg_logic(net_cat[1], listings.spam_keys)
-        self._logger.info('Spam incidents moved: {}'.format(spam_cat[0]))
-        self._move(settings.spam_service_id, spam_cat[0], settings.csa_group_id, self.eid)
-        buckets['spam'] = spam_cat[0]
+            net_cat = self.l.reg_logic(mal_cat[1], listings.netabuse_keys)
+            self._logger.info('Netabuse incidents moved: {}'.format(net_cat[0]))
+            self._move(settings.net_service_id, net_cat[0], settings.csa_group_id, self.eid)
+            buckets['netabuse'] = net_cat[0]
 
-        close_cat = self.l.reg_logic(spam_cat[1], listings.close_keys)
-        self._logger.info('Tickets being closed: {}'.format(close_cat[0]))
-        for ticket in close_cat[0]:
-            self.i.ticket_close(ticket)
-        buckets['close'] = close_cat[0]
+            spam_cat = self.l.reg_logic(net_cat[1], listings.spam_keys)
+            self._logger.info('Spam incidents moved: {}'.format(spam_cat[0]))
+            self._move(settings.spam_service_id, spam_cat[0], settings.csa_group_id, self.eid)
+            buckets['spam'] = spam_cat[0]
 
-        self.leftovers(self.abuse_id, close_cat[1], self.abuse_group, self.eid)
-        self._logger.info('Leftover tickets: {}'.format(close_cat[1]))
+            close_cat = self.l.reg_logic(spam_cat[1], listings.close_keys)
+            self._logger.info('Tickets being closed: {}'.format(close_cat[0]))
+            for ticket in close_cat[0]:
+                self.i.ticket_close(ticket)
+            buckets['close'] = close_cat[0]
 
-        self.i.the_closer()
+            self.leftovers(self.abuse_id, close_cat[1], self.abuse_group, self.eid)
+            self._logger.info('Leftover tickets: {}'.format(close_cat[1]))
+
+        except Exception as e:
+            self._logger.error('Unable to complete Categorizer: {}'.format(e.message))
+
+        finally:
+            self.i.the_closer()
 
     def _move(self, service_id, move_list, groupid, eid):
         """
