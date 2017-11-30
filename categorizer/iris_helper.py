198387bb59b2c43e9e641f5d2522a57d6beba96c
Patrick McConnell <pmcconnell@godaddy.com>
2017-11-30 13:21:35 -0800

    Note can just not exist in prod, capturing

--- categorizer/iris_helper.py
+++ categorizer/iris_helper.py
@@ -80,10 +80,10 @@ class IrisHelper:
         notes_text = self._client.service.GetIncidentCustomerNotes(iid, 0)
         note_dict = xmltodict.parse(notes_text)
 
+        note = ''
+
         if note_dict['NotesByIncident']['Notes']['Item']['@Note']:
             note = note_dict['NotesByIncident']['Notes']['Item']['@Note'] or ''
-        else:
-            note = ''
 
         return email, subject, note
 
